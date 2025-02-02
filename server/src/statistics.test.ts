90503c2b9f7c34a3639ac6591ecfb3c7c75d9f66
GaboFlo <florian.gaboriaud17@gmail.com>
2025-02-02 20:27:20 +0100

    feat: Update dateRange to use Unix timestamps for better compatibility

--- server/src/statistics.test.ts
+++ server/src/statistics.test.ts
@@ -31,7 +31,10 @@ describe("analyseHourByHourBySeason", () => {
     ];
     const result = analyseHourByHourBySeason({
       data,
-      dateRange: [new Date("2020-01-10"), new Date("2025-01-11")],
+      dateRange: [
+        new Date("2020-01-10").getTime(),
+        new Date("2025-01-11").getTime(),
+      ],
     });
 
     const expected = [
