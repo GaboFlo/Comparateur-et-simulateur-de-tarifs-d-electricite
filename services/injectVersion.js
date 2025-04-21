aa60d23ae9d9503568afdd7d5b0ad67f9758d645
GaboFlo <florian.gaboriaud17@gmail.com>
2025-04-21 23:04:46 +0200

    chore: Update deployments.yml to inject version into index.html using command-line argument

--- services/injectVersion.js
+++ services/injectVersion.js
@@ -1,8 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
+// Get the version from command-line arguments
+const args = process.argv.slice(2);
+const version = args[0]; // First argument passed to the script
+
+if (!version) {
+  console.error("Error: Version parameter is required.");
+  process.exit(1);
+}
+
 const indexPath = path.resolve(__dirname, "..", "build", "index.html");
-const version = process.env.REACT_APP_VERSION;
 
 fs.readFile(indexPath, "utf8", (err, data) => {
   if (err) {
