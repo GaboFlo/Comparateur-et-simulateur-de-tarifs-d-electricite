650208d07057a829e36c30f6ac2d8b8d774d3abb
GaboFlo <florian.gaboriaud17@gmail.com>
2025-04-21 22:45:23 +0200

    chore: Update injectVersion script to use JavaScript instead of TypeScript

---
+++ services/injectVersion.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const indexPath = path.resolve(__dirname, "build", "index.html");
+const version = process.env.REACT_APP_VERSION;
+
+fs.readFile(indexPath, "utf8", (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  const result = data.replace(
+    /<title>[^<]*<\/title>/,
+    `$&<meta name="version" content="${version}">`
+  );
+
+  fs.writeFile(indexPath, result, "utf8", (err) => {
+    if (err) return console.log(err);
+    console.log("Version injected into index.html");
+  });
+});
