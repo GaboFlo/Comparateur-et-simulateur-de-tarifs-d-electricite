42040215b844b6797faaf095e52d5ddd30e43ceb
GaboFlo <67909708+GaboFlo@users.noreply.github.com>
2025-08-15 15:12:39 +0200

    🧑‍💻 Ajout de vite (#118)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
